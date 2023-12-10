714a28ccb1b5b052a56f06e3aead2c4aa62b1e73
d-vasily <dvs15m315@yandex.ru>
2023-12-10 13:06:48 +0300

    app 15

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ targets = ['open']
 
 @st.cache_data(ttl=60*60*24)
 def load_data(uri):
-    data = pd.read_csv(uri).rename({'timestamp': 'date'}, axis=1).sort_values(['date']).reset_index(drop=True)
+    data = (pd.read_csv(uri).rename({'timestamp': 'date'}, axis=1).sort_values(['date'])\
+            .tail(365*10).reset_index(drop=True))
     return data
 
 
@@ -118,71 +119,3 @@ for metric in sorted(d_metrics.keys()):
     value = d_metrics[metric](y_true, y_pred)
     st.write(f'{metric} на рассматриваемом периоде:', value)
 
-
-#
-#
-# # Загрузка данных
-# file_path = 'data.csv'  # Укажите путь к вашему CSV файлу
-# data = pd.read_csv(file_path)
-#
-# # Заголовок приложения
-# st.title('Разведочный анализ данных')
-#
-# # Отображение первых строк данных
-# st.subheader('Первые строки данных')
-# st.write(data.head())
-#
-#
-# # Описательные статистики
-# st.subheader('Описательные статистики')
-# st.write(data.describe())
-#
-# st.subheader('Уникальные значения и их количество')
-# l_columns = []
-# for col in data.columns:
-#     if data[col].nunique() < 10:
-#         l_columns.append(col)
-# selected_column = st.selectbox('Выберите колонку', l_columns)
-# st.write(data[selected_column].value_counts())
-#
-#
-#
-# # Графики распределений признаков
-# st.subheader('Графики распределений признаков')
-#
-# # Выберите колонну для построения гистограммы
-# selected_column = st.selectbox('Выберите таргет для построения прогноза', targets)
-# values = st.slider('Select a range of values', 0.0, 100.0, (25.0, 75.0))
-# fig, ax = plt.subplots(figsize=(8, 6))
-# sns.histplot(data[selected_column], kde=True, ax=ax)
-# plt.grid()
-# st.pyplot(fig)
-#
-# # Матрица корреляций
-# st.subheader('Матрица корреляций')
-# fig, ax = plt.subplots(figsize=(10, 8))
-# sns.heatmap(data.corr(), annot=True, cmap='coolwarm', fmt='.2f', ax=ax)
-# st.pyplot(fig)
-#
-# # Графики зависимостей целевой переменной и признаков
-# st.subheader('Scatterplot целевой переменной и признаков')
-#
-# # Выбираем признак для оси y
-# selected_column_y = st.selectbox('Выберите признак для оси y', data.columns)
-#
-# # Создаем график с осью x как 'TARGET' и осью y как выбранный признак
-# fig, ax = plt.subplots(figsize=(8, 6))
-# sns.scatterplot(x='TARGET', y=selected_column_y, data=data, ax=ax)
-# st.pyplot(fig)
-#
-# # Boxplots для сравнения различных категорий
-# st.subheader('Boxplots признаков с делением по TARGET')
-#
-# # Выбор колонны для сравнения
-# selected_column_boxplot = st.selectbox('Выберите колонку для построения Boxplot', data.columns)
-#
-# fig, ax = plt.subplots(figsize=(8, 6))
-# sns.boxplot(x='TARGET', y=selected_column_boxplot, data=data, ax=ax)
-# st.pyplot(fig)
-
-# streamlit run main.py
